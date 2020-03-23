2ffaf8689e668690b8bf5448602003f73421b646
acengukfah <acengukfah@gmail.com>
2020-03-23 14:35:15 +0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool isOver = false;
+    public float restartDelay = 1f;
+    public GameObject levelCompletedUI;
+
+    public void LevelComplete()
+    {
+        levelCompletedUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (!isOver) 
+        { 
+            isOver = true;
+            Debug.Log("GameOver");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500;
+    public bool leftKeys = false;
+    public bool rightKeys = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ((Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow))))
+        {
+            leftKeys = true;
+        }else
+        {
+            leftKeys = false;
+        }
+
+        if ((Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow))))
+        {
+            rightKeys = true;
+        }else 
+        { 
+            rightKeys = false;
+        }
+
+        if (rightKeys)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (leftKeys)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
